c7da16932467c9b3701d23dd22f04ab3231a8224
Prajwal <prajwalsandeep@elementora.com>
2018-03-25 01:36:41 +0530

    Deleted a file that wasn't required.

--- app/src/main/java/com/possystems/kingcobra/redditer/MainActivity.java
+++ app/src/main/java/com/possystems/kingcobra/redditer/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//
         list = findViewById(R.id.list);
         weakReference = new WeakReference<>(this);
         weakReferenceList = new WeakReference<>(list);
